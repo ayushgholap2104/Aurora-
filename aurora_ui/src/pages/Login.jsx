adb3c83d1c948ac1bf09f1dfeb99dd4fff82378e
ayushgholap2104 <ayushgholap2104@gmail.com>
2026-07-24 19:27:51 +0530

    Updated Code

--- aurora_ui/src/pages/Login.jsx
+++ aurora_ui/src/pages/Login.jsx
@@ -1,36 +1,36 @@
 import { useState } from "react";
-import "../styles/login.css"
+import "../styles/login.css";
+import "../styles/responsive.css"
 function Login() {
   return (
     <>
-      <main className="flex justify-center items-center text-center h-screen w-full">
-        <form autoComplete="off">
-          <div className="h-95 w-100 flex flex-col rounded-4xl text-center items-center justify-center shadow-2xl bg-gray-400 gap-12">
-            <h2 className="font-medium text-4xl">Sign in</h2>
-            <div className="items-center rounded-2xl shadow-2xl bg-white flex justify-center gap-2 ">
-              <i className="fa-solid fa-envelope-open text-2xl ml-3 "></i>
+      <main class="Sign_in">
+        <form autocomplete="off">
+          <div class="signin">
+            <h2>Sign in</h2>
+            <div class="E-mail" id="user-up">
+              <i class="fa-solid fa-envelope-open"></i>
               <input
                 id="email"
-                autoComplete="off"
+                autocomplete="off"
                 type="email"
                 name="email"
                 placeholder="Email"
                 required
-                className="h-11 w-65 border-none outline-none text-2xl placeholder:"
               />
             </div>
-            <div className="Password" id="user-up">
-              <i className="fa-solid fa-lock"></i>
+            <div class="Password" id="user-up">
+              <i class="fa-solid fa-lock"></i>
               <input
                 id="password"
-                autoComplete="off"
+                autocomplete="off"
                 type="password"
                 name="password"
                 placeholder="Password"
                 required
               />
             </div>
-            <div className="create_button">
+            <div class="create_button">
               <button type="submit" id="btn">
                 Sign in
               </button>
--- aurora_ui/src/pages/Signup.jsx
+++ aurora_ui/src/pages/Signup.jsx
@@ -1,9 +1,59 @@
-import { useState } from 'react'
-import "../styles/signup.css"
+import { useState } from "react";
+import "../styles/signup.css";
+import "../styles/responsive.css";
 
-function Signup(){
+function Signup() {
   <>
-  </>
+    <main class="Sign_up">
+      <form autocomplete="off">
+        <div class="signup">
+          <h2>Sign Up</h2>
+          <div class="name" id="user-up">
+            <i class="fa-solid fa-circle-user"></i>
+            <input
+              id="name"
+              autocomplete="off"
+              type="text"
+              name="username"
+              placeholder="Name"
+              required
+              style="text-transform: capitalize"
+            />
+          </div>
+          <div class="E-mail" id="user-up">
+            <i class="fa-solid fa-envelope-open"></i>
+            <input
+              id="email"
+              autocomplete="off"
+              type="email"
+              name="email"
+              placeholder="Email"
+              required
+            />
+          </div>
+          <div class="Password" id="user-up">
+            <i class="fa-solid fa-lock"></i>
+            <input
+              id="password"
+              autocomplete="off"
+              type="password"
+              name="password"
+              placeholder="Password"
+              required
+            />
+          </div>
+          <div class="create_button">
+            <button type="submit" id="btn">
+              Create Account
+            </button>
+          </div>
+          <p>
+            Already have an account? <a href="./index.html">Sign in</a>
+          </p>
+        </div>
+      </form>
+    </main>
+  </>;
 }
 
-export default Signup
+export default Signup;
